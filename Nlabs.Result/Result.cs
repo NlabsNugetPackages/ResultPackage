5d2445abf7f8eb92870b167983d5375813c493ee
Cuma KÖSE <turkmvc@gmail.com>
2024-02-27 22:50:59 +0300

    Add project files.

---
+++ Nlabs.Result/Result.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace Nlabs.Result;
+public sealed class Result<T>
+{
+    public T? Data { get; set; }
+    public List<string>? ErrorMessages { get; set; }
+    public bool IsSuccessful { get; set; } = true;
+    public int StatusCode { get; set; } = (int)HttpStatusCode.OK;
+
+    public Result(T data)
+    {
+        Data = data;
+    }
+
+    public Result(int statusCode, List<string> errorMessages)
+    {
+        IsSuccessful = false;
+        StatusCode = statusCode;
+        ErrorMessages = errorMessages;
+    }
+
+    public Result(int statusCode, string errorMessage)
+    {
+        IsSuccessful = false;
+        StatusCode = statusCode;
+        ErrorMessages = new() { errorMessage };
+    }
+
+    public static implicit operator Result<T>(T data)
+    {
+        return new(data);
+    }
+
+    public static implicit operator Result<T>((int statusCode, List<string> errorMessages) parameters)
+    {
+        return new(parameters.statusCode, parameters.errorMessages);
+    }
+
+    public static implicit operator Result<T>((int statusCode, string errorMessage) parameters)
+    {
+        return new(parameters.statusCode, parameters.errorMessage);
+    }
+}
